aabcc24370825507f906deea6e11f38bbf780634
msk007 <krawiec515@gmail.com>
2015-09-10 22:45:10 -0400

    update

--- movie-lib.py
+++ movie-lib.py
@@ -1,21 +1,29 @@
-class ClassName(object):
-    """docstring for """
-    def __init__(self, arg):
-        super(, self).__init__()
-        self.arg = arg
+##class ClassName(object):
+    #"""docstring for """
+    #def __init__(self, arg):
+        #super(, self).__init__()
+        #self.arg = arg
 
 
 
 
 
 class User(object):
-
+        def __init__(self,user_id, arg):
+            self.id_num=user_id
 
 
 
 class Movie(object):
-
+        def __init__(self, args):
+            self.movie_id = ''
+            self.name = ''
+            self.release_date = ''
+            self.genres = []
 
 
 
 class Rating(object):
+        def __init__:(self, user_id, item_id)
+            self.user_id = user_id
+            self.item_id = item_id
