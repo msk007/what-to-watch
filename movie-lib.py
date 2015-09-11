45d9aab1f49975176c9c94a40a863b38abe75fde
msk007 <krawiec515@gmail.com>
2015-09-11 15:14:13 -0400

    changed some things around.
    added a few things and re arranged

--- movie-lib.py
+++ movie-lib.py
@@ -3,27 +3,32 @@
     #def __init__(self, arg):
         #super(, self).__init__()
         #self.arg = arg
+all_movies={}
+all_users={}
 
 
 
 
 
-class User(object):
-        def __init__(self,user_id, arg):
+class User:
+        def __init__(self,user_id):
             self.id_num=user_id
+            all_users[self.id]=self
 
 
-
-class Movie(object):
+class Movie:
         def __init__(self, args):
             self.movie_id = ''
             self.name = ''
             self.release_date = ''
             self.genres = []
+            all_movies{self.id]=self}
+        def rating(self):
+            return self.ratings.values()
 
-
-
-class Rating(object):
-        def __init__:(self, user_id, item_id)
+class Rating:
+        def __init__(self, user_id, movie_id, stars)
             self.user_id = user_id
-            self.item_id = item_id
+            self.item_id = movie_id
+            self.item_id = stars
+            all_movies[self.movie_].add_rating[self]
