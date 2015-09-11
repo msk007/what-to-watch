a6fc693bba10435a49b69ac33f4670da3606a75a
msk007 <krawiec515@gmail.com>
2015-09-10 20:51:21 -0400

    formed classes
    made user, move and rating class

---
+++ movie-lib.py
@@ -0,0 +1,21 @@
+class ClassName(object):
+    """docstring for """
+    def __init__(self, arg):
+        super(, self).__init__()
+        self.arg = arg
+
+
+
+
+
+class User(object):
+
+
+
+
+class Movie(object):
+
+
+
+
+class Rating(object):
