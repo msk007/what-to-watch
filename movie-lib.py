f017d67334ac7e1aee2e7fc73b7a1b13f5d1edfa
msk007 <krawiec515@gmail.com>
2015-09-13 23:29:40 -0400

    update

--- movie-lib.py
+++ movie-lib.py
@@ -5,6 +5,8 @@
         #self.arg = arg
 all_movies={}
 all_users={}
+import math
+import csv
 
 
 
@@ -23,11 +25,12 @@ class User:
             self.ratings[rating.movie]=rating.score
 
 class Movie:
-        def __init__(self,**kwargs):
-            self.movie_id = ''
-            self.name = ''
-            self.genres = []
-            all_movies{self.id] = self}
+        def __init__(self,name, movie_id):
+            self.id=movie_id
+            self.name = name
+            self.genres = genres
+            all_movies[self.id] = self
+            self.ratings={}
         def rating(self):
             return self.ratings.values()
         def __repr__(self):
@@ -36,7 +39,29 @@ class Movie:
             return sorted(self.ratings.values(),reverse=True)
         def get_movie_title(self):
             return all_movies[self.id].title
+        def get_movies():
+            with open('ml-100k/u.item', encoding='latin_1') as f:
+                reader =csv.DictReader(f, fieldnames=['movie_id', 'movie_title' ], delimiter='|')
+                for row in reader:
+                    Movie(int(row['movie_id']), row['movie_title'])
 
+        def euclidean_distance(list_1, list_2):
+            if len(list_1) is 0:
+                return 0
+            differences =[list_1[idx] - list_2[idx] for idx in range(len(list_1))]
+            squares =[diff ** 2 for diff in differences]
+            sum_of_squares =sum(squares)
+                return 1/(1 + math.sqrt(sum_of_squares))
+
+        def load_ratings():
+            with open('ml-100k/u.data') as f:
+                reader =csv.DictReader(f, fieldnames=['user_id', 'item_id', 'rating'], delimiter='\t')
+                for row in reader:
+                    Rating(int(row['user_id']), int(row['item_id']), int(row['rating']))
+        def load_data():
+            load_movies()
+            load_users()
+            load_ratings()
 
 class Rating:
         def __init__(self, user_id, movie_id, stars):
